15c7cc2b0a6985f47574e9a5c6d56b0b3a29ff30
Marina Yurkevich <marina.yurkev@gmail.com>
2019-10-07 16:23:04 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let matchConfigNumbers = [];
+    for (let i = 0; i < str.length; i++) {
+        let symbol = str.charAt(i);
+        for (let ConfigNumber = 0; ConfigNumber < bracketsConfig.length; ConfigNumber++) {
+            let openBracketConfig = bracketsConfig[ConfigNumber][0];
+            let closeBracketConfig = bracketsConfig[ConfigNumber][1];
+            if (symbol === openBracketConfig) {
+                if (openBracketConfig === closeBracketConfig && ConfigNumber === matchConfigNumbers[matchConfigNumbers.length - 1]) {
+                    matchConfigNumbers.pop();
+                } else {
+                    matchConfigNumbers.push(ConfigNumber);
+                }
+            } else if (symbol === closeBracketConfig) {
+                if (ConfigNumber === matchConfigNumbers[matchConfigNumbers.length - 1]) {
+                    matchConfigNumbers.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+    }
+    return !matchConfigNumbers.length;
 }
